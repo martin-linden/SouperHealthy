3d848e3a9499c45b5eda68d0992ea1a9e0179c56
martin-linden <martin.linden@student.kyh.se>
2020-10-19 11:04:52 +0200

    order overview ngfoor loop works from json

--- src/app/pages/order-overview/order-overview.component.ts
+++ src/app/pages/order-overview/order-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import SoupJson from '../../../assets/soups.json';
 
 @Component({
   selector: 'app-order-overview',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderOverviewComponent implements OnInit {
 
-  constructor() { }
+  soups: any
+
+  constructor() { 
+    this.soups = SoupJson;
+  }
 
   ngOnInit() {
   }
