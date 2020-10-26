826c64a79c8469f5fd4a2c3b1bf3d755b13a4783
martin-linden <martin.linden@student.kyh.se>
2020-10-26 13:31:57 +0100

    flow wizard colors added on page change

--- src/app/components/wizard/wizard.component.ts
+++ src/app/components/wizard/wizard.component.ts
@@ -6,6 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wizard.component.scss']
 })
 export class WizardComponent implements OnInit {
+  pathname: string;
+  circleOne: string;
+  circleTwo: string;
+
+  
+  
+
+  firstCircle(){
+     if(window.location.pathname === '/delivery-info' || window.location.pathname === '/order-overview' || window.location.pathname === '/confirmation'){
+      return '#32936f'
+    } else{
+      return '#bcbcbc'
+    }
+  }
+  secondCircle(){
+     if(window.location.pathname === '/order-overview' || window.location.pathname === '/confirmation' ){
+      return '#32936f'
+    } else{
+      return '#bcbcbc'
+    }
+  }
+  thirdCircle(){
+     if(window.location.pathname === '/confirmation'){
+      return '#32936f'
+    } else{
+      return '#bcbcbc'
+    }
+  }
+
+
+ 
 
   constructor() { }
 
